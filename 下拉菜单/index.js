9c95fa0373653689d73f674ef2f98e8e0db3a2f7
FovNartuo <45628596+FovNartuo@users.noreply.github.com>
2018-12-14 22:46:08 +0800

    dropdown

---
+++ 下拉菜单/index.js
@@ -0,0 +1,15 @@
+function displaySubMenu(li) {
+ 
+    var subMenu = li.getElementsByTagName("ul")[1];
+ 
+    subMenu.style.display = "block";
+ 
+}
+ 
+function hideSubMenu(li) {
+ 
+    var subMenu = li.getElementsByTagName("ul")[1];
+ 
+    subMenu.style.display = "none";
+  
+}
