38e6507141514d3bdec1fa888a69ebb49e936550
WenDong Zuo <45628596+FovNartuo@users.noreply.github.com>
2019-01-29 21:24:22 +0800

    侧边栏内容区JS代码特效

--- 侧边栏导航/script.js
+++ 侧边栏导航/script.js
@@ -1,9 +1,39 @@
 (function(){
+
+    var Menubar = function(){
+        this.el = document.querySelector("#sidebar ul");
+        this.state = "allClosed";   //hasOpened
+        this.el.addEventListener("click",function(e){
+            //阻止冒泡
+            e.stopPropagation();            
+        });
+        var self = this;
+        this.currentOpenedMenuContent = null;
+        this.menuList = document.querySelectorAll("#sidebar ul > li");
+        for(var i=0; i<this.menuList.length; i++){
+            this.menuList[i].addEventListener("click",function(e){
+                var menuContentEl = document.getElementById(e.currentTarget.id + "-content");
+                // console.log(menuContentEl);
+                if(self.state === "allClosed"){
+                    console.log("打开了"+menuContentEl.id);
+                    self.state = "hasOpened";
+                    self.currentOpenedMenuContent = menuContentEl;
+                }else{
+                    console.log("关闭了"+self.currentOpenedMenuContent.id);
+                    console.log("打开了"+menuContentEl.id);
+                    self.state = "hasOpened";
+                    self.currentOpenedMenuContent = menuContentEl;
+                }
+            })
+        }
+    }
+
     var Sidebar = function(eId,closeBarId){
         this.state = "opend";
         this.el = document.getElementById(eId || "sidebar");
         this.closeBarEl = document.getElementById(closeBarId || "closebar");
         var self = this;
+        this.menubar = new Menubar();
         this.el.addEventListener("click",function(event){
             if(event.target !== self.el){
                 self.triggerSwitch();
@@ -11,12 +41,14 @@
         })
     };
     Sidebar.prototype.close = function(){
-        console.log("关闭了sidebar");
-        this.state = "opend";
+        // console.log("关闭了sidebar");
+        this.el.className = "sidebar-move-left";
+        this.closeBarEl.className = "closeBar-move-right";
+        this.state = "closed";
     };
     Sidebar.prototype.open = function(){
-        console.log("打开了sidebar");
-        this.state = "closed";
+        // console.log("打开了sidebar");
+        this.state = "opend";
     };
     Sidebar.prototype.triggerSwitch = function(){
         if(this.state === "opend"){
@@ -26,5 +58,7 @@
         }
     };
     var sidebar = new Sidebar();
+
+    
 
 })();
